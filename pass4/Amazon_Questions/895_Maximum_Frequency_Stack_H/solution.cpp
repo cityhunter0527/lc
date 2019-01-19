a46dafb59de3d3875094608cc1c9f5424c82c171
Yaming Kuang <yaming.kuang@gmail.com>
2019-01-19 11:09:13 +0800

    fix

--- pass4/Amazon_Questions/895_Maximum_Frequency_Stack_H/solution.cpp
+++ pass4/Amazon_Questions/895_Maximum_Frequency_Stack_H/solution.cpp
@@ -55,19 +55,95 @@ The total number of FreqStack.push and FreqStack.pop calls will not exceed 15000
 //      mapheap.insert(element, map[element]) // add the element back to the maxheap
 // else go to next node in the double linked list;
 //
+
+#include <iostream>
+#include <queue>
+#include <deque>
+#include <map>
+#include <unordered_map>
+#include <vector>
+#include <cassert>
+
 class FreqStack {
+
 public:
     FreqStack() {
 
     }
 
     void push(int x) {
+        // 1. push x to deque;
+        m_deque.emplace_front(x);
 
+        // 2. update x's occurences in m_map
+        m_map[x]++;
+
+        // 3. update x in multimap
+        if (m_map[x] > 1) {
+            // 3.a if x has been inserted to multimap before, first remove it from its previous num_of_occurrences;
+            auto range = m_multimap.equal_range(m_map[x] - 1);
+            for (auto it = range.first; it != range.second; it++) {
+                if (it->second == x) {
+                    // Note: no need to update maxheap even though currently x's occurrences is the max one in heap (say x is increasing from 5 to 6 with this push), 
+                    // because we are doing push, and x's occurences will eventually drop to 5 when doing pop();
+                    // We only need to remove the top of the maxheap when we are doing pop()
+                    m_multimap.erase(it);
+                    break;
+                }
+            }
+        }
+        
+        // 3.b insert x to new place;
+        m_multimap.insert(std::make_pair(m_map[x], x));
+        
+        // 4. insert x's num_of_occurences into max_heap, if the number already exists, the maxheap should ignore this insert;
+        m_max_heap.push(m_map[x]);
     }
-
+    
     int pop() {
+        int res = 0;
+        for (auto it = m_deque.begin(); it != m_deque.end(); it++) {
+            if (m_max_heap.top() == m_map[*it]) {
+                // the element in deque has the num_of_occurences equal to the one on top of maxheap;
+                res = *it;
+                // 1. remove it from deque;
+                m_deque.erase(it);
+ 
+                assert(m_map[res] > 0);
+                // 2. update m_map
+                // It is okay to decrease m_map[res] to 0 if res doesn't exist anymore;
+                m_map[res]--;
+                
+                // 3. remove res from its original num_of_occurences in multimap;
+                auto range = m_multimap.equal_range(m_max_heap.top());
+                for (auto it = range.first; it != range.second; it++) {
+                    if (it->second == res) {
+                        m_multimap.erase(it);
+                        break;
+                    }
+                }
+                
+                // 4. update the max heap;
+                // 4.a first, add the decreased num_of_occurences into the heap
+                if (m_map[res] > 0) m_max_heap.push(m_map[res]);
+                
+                // 4.b if this is the last element of the max num_of_occurences being poped, remove the top value in the heap;
+                if (m_multimap.count(m_max_heap.top()) == 0)  {
+                    // no element left in this num_of_occurences, remove the top element in maxheap;
+                    m_max_heap.pop();
+                }
 
+                break;
+            }
+        } 
+        return res;
     }
+
+private:
+    std::unordered_multimap<int, int>   m_multimap; // multimap of num_of_occurences ==> list of elements with same # of occurrences;
+    std::unordered_map<int, int>        m_map;      // map of elements ==> # of occurences;
+    std::deque<int>                     m_deque;    // deque of pushed element 
+    std::priority_queue<int>            m_max_heap; // by default, it is maxheap
 };
 
 /**
@@ -78,5 +154,19 @@ public:
  */
 
 int main() {
+    FreqStack s;
+    s.push(5);
+    s.push(7);
+    s.push(5);
+    s.push(7);
+    s.push(4);
+    s.push(5);
+    
+    std::cout << s.pop() << "\n";
+    std::cout << s.pop() << "\n";
+    std::cout << s.pop() << "\n";
+    std::cout << s.pop() << "\n";
+    std::cout << s.pop() << "\n";
+    std::cout << s.pop() << "\n";
     return 0;
 }
