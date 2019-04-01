69d5fa94a94e6c736b362a91444ddecb99b5fcb8
Yaming Kuang <yaming.kuang@gmail.com>
2019-04-01 19:19:16 +0800

    fix

--- pass4/0_dfs/210_Course_Schedule_II_M/solution.cpp
+++ pass4/0_dfs/210_Course_Schedule_II_M/solution.cpp
@@ -31,14 +31,117 @@ You may assume that there are no duplicate edges in the input prerequisites.
 
 #include <iostream>
 #include <vector>
+#include <stack>
+#include <list>
+#include <unordered_set>
+
+enum class Color {white, grey, black};
 
 class Solution {
 public:
     std::vector<int> findOrder(int numCourses, std::vector<std::pair<int, int>>& prerequisites) {
+        std::vector<int>            res;
+        std::vector<Color>          v(numCourses, Color::white);      // visited
+        std::vector<std::list<int>> edges(numCourses);
+        std::vector<int>            indegrees(numCourses, 0);
+        std::stack<int>             s;      // order
 
+        for (auto& x : prerequisites) {
+            edges[x.first].push_back(x.second);
+            indegrees[x.second]++;
+        }
+        // dfs
+        for (uint32_t i = 0; i < numCourses; i++) {
+            if (indegrees[i]) { continue; }
+            s.push(i);
+            v[i] = Color::grey;
+            while (!s.empty()) {
+                int j = s.top();
+                if (edges[j].empty()) {
+                    res.push_back(j);    
+                    s.pop();
+                    v[j] = Color::black;
+                } else {
+                    int k = edges[j].front();
+                    if (v[k] == Color::grey) {
+                        res.clear();
+                        return res;
+                    } else if (v[k] == Color::white) {
+                        s.push(k);
+                        v[k] = Color::grey;
+                    } 
+                    edges[j].pop_front();
+                }
+            }
+        }
+        // 
+        // if there is loop, it is possible that every node in this loop has in-degree > 0, so this loop never got chance to be iterated;
+        // so if the result doesn't contain all the nodes, it must be this case, so return empty;
+        // e.g. 
+        //    _________
+        //  \|/        |
+        //   1 -> 0 -> 2
+        //   3 -> 4
+        //
+        if (res.size() != numCourses) {
+            res.clear();
+        }
+        return res;
     }
 };
 
+void print(std::vector<int> v) {
+    for (auto x : v) {
+        std::cout << x << ", ";
+    }
+    std::cout << std::endl;
+}
 int main () {
+    Solution s;
+    std::vector<std::pair<int, int>> v;
+    
+    v.push_back(std::make_pair(1,0));
+    print(s.findOrder(2, v));
+#if 0 
+    // 
+    //  1 -> 0
+    // /|\  /|\
+    //  3 -> 2
+    //
+    v.clear();
+    v.push_back(std::make_pair(1,0));
+    v.push_back(std::make_pair(2,0));
+    v.push_back(std::make_pair(3,1));
+    v.push_back(std::make_pair(3,2));
+    print(s.findOrder(4, v));
+
+    // 1-> 0 -> 3
+    //         \|/
+    //       -> 2 -> 5 -> 6
+    //         \|/  /|\
+    //          4 ----
+    v.clear();
+    v.push_back(std::make_pair(1,0));
+    v.push_back(std::make_pair(0,3));
+    v.push_back(std::make_pair(0,2));
+    v.push_back(std::make_pair(3,2));
+    v.push_back(std::make_pair(2,5));
+    v.push_back(std::make_pair(4,5));
+    v.push_back(std::make_pair(5,6));
+    v.push_back(std::make_pair(2,4));
+    print(s.findOrder(7, v));
+#endif
+    // 
+    // [[1,0],[2,6],[1,7],[5,1],[6,4],[7,0],[0,5]]
+    //
+    v.clear();
+    v.push_back(std::make_pair(1,0));
+    v.push_back(std::make_pair(2,6));
+    v.push_back(std::make_pair(1,7));
+    v.push_back(std::make_pair(5,1));
+    v.push_back(std::make_pair(6,4));
+    v.push_back(std::make_pair(7,0));
+    v.push_back(std::make_pair(0,5));
+    print(s.findOrder(8, v));
     return 1;
 }
