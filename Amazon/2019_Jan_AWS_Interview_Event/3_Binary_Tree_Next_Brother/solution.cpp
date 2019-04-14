70218418648a518e3ab7ad153dde624b4a1d4b60
Yaming Kuang <yaming.kuang@gmail.com>
2019-04-14 20:26:51 +0800

    fix

--- Amazon/2019_Jan_AWS_Interview_Event/3_Binary_Tree_Next_Brother/solution.cpp
+++ Amazon/2019_Jan_AWS_Interview_Event/3_Binary_Tree_Next_Brother/solution.cpp
@@ -1,4 +1,6 @@
-/*
+/* 
+ * 116. Populating Next Right Pointers in Each Node
+ *
  * Given a binary tree, link the nodes in same level. 
  *
  * Example, Input 
@@ -20,24 +22,84 @@
  * Assumption: the binary tree is a full tree;
  *
  * Plus: Can you solve it if the tree is not a full binary tree?
+ * Plus: Can you do it recursively?
  * */
+#include <queue>
+#include <iostream>
 
-class TreeNode {
+class Node {
 public:
     int val;
-    TreeNode* left;
-    TreeNode* right;
-    TreeNode* next;
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr), next(nullptr) {}
+    Node* left;
+    Node* right;
+    Node* next;
+    Node(int x) : val(x), left(nullptr), right(nullptr), next(nullptr) {}
 };
 
-class solution {
+// 
+// solution-1: level order traversal 
+// solution-2: recrusive 
+//
+class Solution {
 public:
-    TreeNode* convert(TreeNode* root) {
-        return nullptr;
+    // given the left_most pointer of current layer, 
+    // build the 'next' pointer of next layer 
+    void build_next(Node* left_most) {
+        if (left_most == nullptr) { return; }
+        auto p = left_most;
+        while (p && p->left) {
+            p->left->next = p->right;   
+            if (p->next) {
+                p->right->next = p->next->left;
+            } else {
+                p->right->next = nullptr;
+            }
+            p = p->next;
+        }
+        build_next(left_most->left);
     }
-}
+    
+    // recursive
+    Node* connect2(Node* root) {
+        if (root == nullptr) { return nullptr; }
+        build_next(root);
+        return root;   
+    }
+
+    // level order
+    Node* connect(Node* root) {
+        if (root == nullptr) { return nullptr; }
+        std::queue<Node*>  q;
+        q.push(root);
+        q.push(nullptr);
+        Node* prev = nullptr;
+        while (!q.empty()) {
+            auto p = q.front();
+            q.pop();
+            if (prev) { 
+                prev->next = p; 
+            }
+            prev = p;
+            if (p) {
+                if (p->left) { q.push(p->left); }
+                if (p->right) { q.push(p->right); }
+            } else if (!q.empty()) {
+                q.push(nullptr);
+            }
+        }
+        return root;
+    }
+};
 
 int main() {
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+    Solution s;
+    root = s.connect(root);
     return 1;
 }
---
+++ pass4/0_dfs/430_Flatten_a_Multilevel_Doubly_Linked_List_M/solution.cpp
@@ -0,0 +1,151 @@
+/*
+ * 430. Flatten a Multilevel Doubly Linked List
+ *
+ * You are given a doubly linked list which in addition to the next and previous pointers, it could have a child pointer, 
+ * which may or may not point to a separate doubly linked list. These child lists may have one or more children of their 
+ * own, and so on, to produce a multilevel data structure, as shown in the example below.
+
+Flatten the list so that all the nodes appear in a single-level, doubly linked list. 
+You are given the head of the first level of the list.
+
+Example:
+
+Input:
+ 1---2---3---4---5---6--NULL
+         |
+         7---8---9---10--NULL
+             |
+             11--12--NULL
+
+Output:
+1-2-3-7-8-11-12-9-10-4-5-6-NULL
+
+
+Explanation for the above example:
+
+Given the following multilevel doubly linked list:
+
+We should return the following flattened doubly linked list:
+ * */
+
+#include <stack>
+#include <iostream>
+
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* prev;
+    Node* next;
+    Node* child;
+
+    Node() {}
+
+    Node(int _val, Node* _prev, Node* _next, Node* _child) {
+        val = _val;
+        prev = _prev;
+        next = _next;
+        child = _child;
+    }
+};
+
+class Solution {
+public:
+    Node* flatten(Node* head) {
+        Node* p = head;
+        while (p && !p->child) {
+            if (p) {
+                
+            } else {
+                
+            }
+        }
+    }
+
+    // iterative with stack
+    Node* flatten.iterative(Node* head) {
+        if (head == nullptr) return head;
+        std::stack<Node*> s;
+        Node* p = head, *prev = nullptr;
+        s.push(nullptr);
+
+        while (!s.empty()) {
+            while (p && !p->child) { 
+                prev = p; 
+                p = p->next; 
+            }
+            if (!p) {
+                p = s.top();
+                prev->next = p; 
+                if (p) {
+                    p->prev = prev;
+                }
+                s.pop();
+            } else {
+                s.push(p->next);
+                p->next = p->child;
+                p->next->prev = p;
+                p->child = nullptr;
+                p = p->next;
+            }
+        }
+
+        return head;
+    }
+};
+
+
+void print(Node* n) {
+    std::cout << "Node: ";
+    Node* prev = nullptr;
+    while (n) {
+        std::cout << n->val << "->";
+        assert(n->child == nullptr);
+        prev = n;
+        n = n->next;
+        if (n) assert(n->prev == prev);
+    }
+    std::cout << "null" << std::endl;
+}
+/*
+ *   
+ 1---2---3---4---5---6--NULL
+         |
+         7---8---9---10--NULL
+             |
+             11--12--NULL
+ * */
+int main() {
+#if 1
+    Node* head = new Node(1, nullptr, nullptr, nullptr);
+    head->next = new Node(2, nullptr, nullptr, nullptr);
+    head->next->prev = head;
+    head->next->next = new Node(3, nullptr, nullptr, nullptr);
+    head->next->next->prev = head->next;
+
+    Node* p1 = new Node(7, nullptr, nullptr, nullptr);
+    head->next->next->child = p1;
+    p1->next = new Node(8, nullptr, nullptr, nullptr);
+    p1->next->prev = p1;
+    p1->next->next = new Node(9, nullptr, nullptr, nullptr);
+    p1->next->next->prev = p1->next;
+    p1->next->next->next = new Node(10, nullptr, nullptr, nullptr);
+    p1->next->next->next->prev = p1->next->next;
+
+    p1->next->child = new Node(11, nullptr, nullptr, nullptr);
+    p1->next->child->next = new Node(12, nullptr, nullptr, nullptr);
+    p1->next->child->next->prev = p1->next->child;
+
+    Node* p2 = new Node(4, nullptr, nullptr, nullptr);
+    head->next->next->next = p2;
+    p2->prev = head->next->next;
+    p2->next = new Node(5, nullptr, nullptr, nullptr);
+    p2->next->prev = p2;
+    p2->next->next = new Node(6, nullptr, nullptr, nullptr);
+    p2->next->next->prev = p2->next;
+#endif
+    Solution s;
+    Node* out = s.flatten(head);
+    print(out);
+    return 1;
+}
