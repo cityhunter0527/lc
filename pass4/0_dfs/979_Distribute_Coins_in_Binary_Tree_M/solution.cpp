3e5daf7f5a4737bba927997157896b7961b949ba
Yaming Kuang <yaming.kuang@gmail.com>
2019-05-26 16:49:36 +0800

    fix

--- pass4/0_dfs/979_Distribute_Coins_in_Binary_Tree_M/solution.cpp
+++ pass4/0_dfs/979_Distribute_Coins_in_Binary_Tree_M/solution.cpp
@@ -1,9 +1,11 @@
 /*  
  *  979. Distribute Coins in Binary Tree
  * 
-Given the root of a binary tree with N nodes, each node in the tree has node.val coins, and there are N coins total.
+Given the root of a binary tree with N nodes, each node in the tree has node.val coins, 
+and there are N coins total.
 
-In one move, we may choose two adjacent nodes and move one coin from one node to another.  (The move may be from parent to child, or from child to parent.)
+In one move, we may choose two adjacent nodes and move one coin from one node to another.  
+(The move may be from parent to child, or from child to parent.)
 
 Return the number of moves required to make every node have exactly one coin.
 
@@ -49,18 +51,61 @@ Note:
 0 <= node.val <= N
  * */
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
+#include <iostream>
+#include <cassert>
+
+/** * Definition for a binary tree node. */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
 class Solution {
 public:
+    // return number of coins needed for this sub-tree
+    int dfs(TreeNode* x) {
+        if (x == nullptr) { 
+            return 0;
+        }
+        return dfs(x->left) + dfs(x->right) + x->val-1;
+    }
+
     int distributeCoins(TreeNode* root) {
-        
+        if (root == nullptr) { 
+            return 0;
+        }
+        // 
+        // 1. from this root node, give dfs(root->left) nodes to left, 
+        //    then give dfs(root->right) nodes to right, so each sub-tree have enough nodes to do distribution.
+        // 2. plus distruteCoins of left sub-tree and
+        // 3. plus distruteCoins of right sub-tree
+        // 
+        return abs(dfs(root->left)) + abs(dfs(root->right)) + distributeCoins(root->left) + distributeCoins(root->right); 
     }
 };
+
+int main() {
+    TreeNode* root = new TreeNode(3);
+    root->left = new TreeNode(0);
+    root->right = new TreeNode(0);
+    Solution s;
+    assert(s.distributeCoins(root) == 2);
+
+    //        5
+    //       /
+    //      0
+    //     /
+    //    0
+    //   / \
+    //  0   0
+    root = new TreeNode(5);
+    root->left = new TreeNode(0);
+    root->left->left = new TreeNode(0);
+    root->left->left->left = new TreeNode(0);
+    root->left->left->right = new TreeNode(0);
+    assert(s.distributeCoins(root) == 9);
+
+    return 1;
+}
